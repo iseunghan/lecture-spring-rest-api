8c561ef93a539501a0fdf75f5cdf1156a0b2f746
iseunghan <tmdgks301@naver.com>
2020-09-11 00:42:50 +0900

    EventTests Runwith JUnitParams for Test code ReFactoring

--- src/test/java/me/iseunghan/demoinflearnrestapi/events/EventTest.java
+++ src/test/java/me/iseunghan/demoinflearnrestapi/events/EventTest.java
@@ -1,10 +1,33 @@
 package me.iseunghan.demoinflearnrestapi.events;
 
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.runner.RunWith;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 //option+ctrl+o -> optimize import
+
+/**
+ * "JUnitParams" : test 중복 코드 제거에 좋음
+ *
+ * @RunWith(JUnitParamsRunner.class)
+ * public class PersonTest {
+ *
+ *   @Test
+ *   @Parameters({"17, false",
+ *                "22, true" })
+ *   public void personIsAdult(int age, boolean valid) throws Exception {
+ *     assertThat(new Person(age).isAdult(), is(valid));
+ *   }
+ *
+ * }
+ */
+@RunWith(JUnitParamsRunner.class)
 class EventTest {
 
     @Test
@@ -32,41 +55,60 @@ class EventTest {
         assertThat(event.getDescription()).isEqualTo(description);
     }
 
-    @Test
-    public void free_무료인지_아닌지() {
+    /*
+    Junit5 에서는 이런식으로 작성해야한다.
+    @ParameterizedTest
+    @CsvSource({
+            "0,0,true",
+            "100, 0, false"
+    })*/
+
+    @ParameterizedTest
+    @MethodSource("parametersForTestFree")
+    public void testFree(int basePrice, int maxPrice, boolean free) {
 
         // Given
         Event event = Event.builder()
-                .basePrice(0)
-                .maxPrice(0)
+                .basePrice(basePrice)
+                .maxPrice(maxPrice)
                 .build();
+
         // When
         event.update();
-        // Then
-        assertThat(event.isFree()).isTrue();
 
-
-        // Given
-        event = Event.builder()
-                .basePrice(100)
-                .maxPrice(0)
-                .build();
-        // When
-        event.update();
         // Then
-        assertThat(event.isFree()).isFalse();
+        assertThat(event.isFree()).isEqualTo(free);
+    }
+
+    // type-safe 하게 메소드로 빼놨다.
+    private static Object[] parametersForTestFree() {
+        return new Object[]{
+                new Object[] {0, 0, true},
+                new Object[] {100, 0, false},
+                new Object[] {0, 100, false}
+        };
     }
 
 
-    @Test
-    public void offline_인지_아닌지() {
+    @ParameterizedTest
+    @MethodSource("parametersForOffline")
+    public void offline_인지_아닌지(String location, boolean offline) {
         // Given
-        Event event = Event.builder().build();
+        Event event = Event.builder()
+                .location(location)
+                .build();
 
         // When
         event.update();
 
         // Then
-        assertThat(event.isOffline()).isFalse();
+        assertThat(event.isOffline()).isEqualTo(offline);
+    }
+
+    private static Object[] parametersForOffline() {
+        return new Object[] {
+                new Object[] {"korea", true},
+                new Object[] {null , false}
+        };
     }
 }
